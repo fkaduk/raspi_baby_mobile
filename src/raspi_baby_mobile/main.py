a2a9f736e31f57b5c259f537087579159bb402a9
fkaduk <->
2025-05-24 12:41:23 +0200

    add error for missing camera
    fix
    
    fix typo

--- src/raspi_baby_mobile/main.py
+++ src/raspi_baby_mobile/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import cv2
 import RPi.GPIO as GPIO
 
 import raspi_baby_mobile
@@ -8,7 +9,7 @@ BUZZER_PIN = 18
 SERVO_PIN = 12
 
 
-def main(poweron_selftest=True):
+def main(camera_index=0, poweron_selftest=True):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup([BUZZER_PIN, SERVO_PIN], GPIO.OUT)
     buzzer = GPIO.PWM(BUZZER_PIN, 2_000)
@@ -16,6 +17,11 @@ def main(poweron_selftest=True):
 
     if poweron_selftest:
         raspi_baby_mobile.poweron_selftest(buzzer, servo)
+
+    cap = cv2.VideoCapture(camera_index)
+    if not cap.isOpened():
+        print(f"❌ Could not open camera {camera_index}")
+        raspi_baby_mobile.buzzer_notification(buzzer, "no_camera")
 
     GPIO.cleanup()
 
