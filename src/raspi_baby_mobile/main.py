731dcf9d2a38b01a20e94343d17c2838571f146d
fkaduk <->
2025-06-08 10:13:03 +0200

    trying to speed things up

--- src/raspi_baby_mobile/main.py
+++ src/raspi_baby_mobile/main.py
@@ -21,6 +21,10 @@ def main(camera_index=0, poweron_selftest=True):
 
         cap = cv2.VideoCapture(camera_index)
         cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        cap.set(cv2.CAP_PROP_FPS, 30)
+        cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         if not cap.isOpened():
             rbm.buzzer_notification(pi, BUZZER_PIN, "no_camera")
             raise SystemExit(f"❌ Could not open camera {camera_index}")
